251f4ee50f8bac7f28a5f8586ea4a72f72fcc3d7
Telrond Rocklaer <germankalnyi@gmail.com>
2024-11-19 13:11:51 +0200

    Functional DiceRoller component, initial setup, basic roll history

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#c19976",       // Muddy Waters
+        secondary: "#d62828ff",   // Fire Engine Red
+        background: "#210124ff",  // Dark Purple
+        text: "#f9dbbdff",        // Light Orange
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
